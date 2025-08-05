380e6ff1382bca39fa71923e53474f4d1beabe07
FenryrMKIII <13049443+FenryrMKIII@users.noreply.github.com>
2025-08-05 14:43:53 +0200

    chore: initial commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devServer: {
+    static: './dist',
+    open: true,
+  },
+  mode: 'development',
+};
